9a4f2ff662b63128ce47350c67c15ea50bc8716c
Sinan Özdemir <>
2018-11-02 14:32:04 +0100

    teststuff

---
+++ src/main/java/vision/diary/diary40/Diary40Application.java
@@ -0,0 +1,12 @@
+package vision.diary.diary40;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Diary40Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Diary40Application.class, args);
+	}
+}
---
+++ src/test/java/vision/diary/diary40/Diary40ApplicationTests.java
@@ -0,0 +1,16 @@
+package vision.diary.diary40;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Diary40ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
